8b762ae5f9a290b5893a766c12e514c1791eaa9c
João Pedro Belai <102523929+JoaoBelai@users.noreply.github.com>
2025-04-14 08:26:35 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -2,10 +2,10 @@ usuario = int(input("Digite um número: "))
 soma = 0
 numero = 0
 
-if usuario > 0:
+if usuario >= 0:
     while numero <= usuario:
         soma = soma + numero
         numero += 1
-    print(f"A soma de todos os números de 1 até {usuario} é: {soma}")
+    print(f"A soma de todos os números até {usuario} é: {soma}")
 else:
     print("INVALIDO")
