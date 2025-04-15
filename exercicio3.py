b3ce8044bcf3c9ea639ed507faadd53457f1378a
João Pedro Belai <102523929+JoaoBelai@users.noreply.github.com>
2025-04-15 16:47:23 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,5 +1,5 @@
 numero = int(input("Digite um número que deseja ver a tabuada: "))
 
-for conta in range(1,11):
+for conta in range(0,11):
     resultado = numero * conta
     print(f"{numero} x {conta} = {resultado}")
